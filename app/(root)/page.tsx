86400fea05a3434f5e13e95d32b915241c9f4527
lichaa824 <lisandrorubi@gmail.com>
2023-05-12 20:15:37 -0300

    ADD: Root Page

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import UrlForm from "../components/UrlForm";
+const Page = () => {
+  return (
+    <div className="flex min-h-full flex-col  items-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+      <UrlForm />
+    </div>
+  );
+};
+
+export default Page;
