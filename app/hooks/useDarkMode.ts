6c266f40a744ce35fe1e10db249c00cbca96e112
lichaa824 <lisandrorubi@gmail.com>
2023-05-12 20:14:00 -0300

    ADD: DarkMode Hook

---
+++ app/hooks/useDarkMode.ts
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+type themes = "light" | "dark";
+
+const useDarkMode = () => {
+  const [theme, setTheme] = useState<themes>("light");
+
+  useEffect(() => {
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    if (prefersDark) {
+      setTheme("dark");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (document.querySelector("html")?.hasAttribute("data-theme")) {
+      document.querySelector("html")?.setAttribute("data-theme", theme);
+    }
+  }, [theme]);
+
+  const toggleTheme = () => {
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+  };
+
+  return { theme, toggleTheme };
+};
+
+export default useDarkMode;
