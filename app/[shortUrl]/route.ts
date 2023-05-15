3e195893aad649bcd1e1cf3ad23945eacc355223
lichaa824 <lisandrorubi@gmail.com>
2023-05-15 20:47:36 -0300

    FIX: bug when protocol specified

--- app/[shortUrl]/route.ts
+++ app/[shortUrl]/route.ts
@@ -1,6 +1,27 @@
 import { redirect } from 'next/navigation';
+import prisma from '../libs/prismadb';
+import { NextResponse, NextRequest } from 'next/server';
 
-export async function GET(request: Request) {
-  const urlToRedirect = request.url.split('http://localhost:3000/')[1];
-  redirect(`//${urlToRedirect}`);
+export async function GET(request: NextRequest) {
+  const origin = request.nextUrl.origin;
+  const shortUrl_string = request.url.split(origin + '/')[1];
+
+  if (shortUrl_string.length !== 7) {
+    redirect('/error/404');
+  }
+
+  const url_db = await prisma.shortURL.findFirst({
+    where: { alias: shortUrl_string },
+  });
+
+  console.log('test: ', url_db?.fullUrl);
+
+  if (url_db) {
+    url_db.fullUrl.startsWith('https://') ||
+    url_db.fullUrl.startsWith('http://')
+      ? redirect(url_db.fullUrl)
+      : redirect(`//${url_db.fullUrl}`);
+  }
+
+  return new NextResponse('Not Found: Not a valid short url', { status: 404 });
 }
