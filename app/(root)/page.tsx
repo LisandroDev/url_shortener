a50866321ea94f7ca9123f7abf836154e7795f63
lichaa824 <lisandrorubi@gmail.com>
2023-05-15 21:45:14 -0300

    ADD: Modal from daisyui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
-import UrlForm from "./components/UrlForm";
+import UrlForm from './components/UrlForm';
+import Modal from '../components/modals/Modal';
+
 const Page = () => {
   return (
-    <div className="flex min-h-full flex-col  items-center py-12 sm:px-6 lg:px-8 base-100">
+    <div className='flex min-h-full flex-col  items-center py-12 sm:px-6 lg:px-8 base-100'>
       <UrlForm />
     </div>
   );
