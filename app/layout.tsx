8bbf9b6642d0e88f599cdf491f94c87c3e44651d
lichaa824 <lisandrorubi@gmail.com>
2023-05-23 14:53:24 -0300

    Add Session Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,26 @@
-import './globals.css'
+import './globals.css';
 
-import Navbar from './components/navbar/Navbar'
+import Navbar from './components/navbar/Navbar';
+import AuthContext from './context/AuthContext';
 
 export const metadata = {
   title: 'URL Shortener',
   description: 'URL Shortener built with Next.JS',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html className="base-100" data-theme="light" lang="en">
-        <body>
-        <Navbar />
-        {children}
-        </body>
+    <html className='base-100' data-theme='light' lang='en'>
+      <body>
+        <AuthContext>
+          <Navbar />
+          {children}
+        </AuthContext>
+      </body>
     </html>
-  )
+  );
 }
