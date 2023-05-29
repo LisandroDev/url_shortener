a656fc16e6e0779b9703885c883ce7854e718d59
lichaa824 <lisandrorubi@gmail.com>
2023-05-29 16:38:09 -0300

    Fix main not taking height remaining

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import Navbar from './components/navbar/Navbar';
 import AuthContext from './context/AuthContext';
 import ToasterContext from './context/ToasterContext';
+import Footer from './components/footer/Footer';
 
 export const metadata = {
   title: 'URL Shortener',
@@ -16,12 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html className='base-100' data-theme='light' lang='en'>
-      <body>
+      <body className='flex h-screen flex-col'>
         <AuthContext>
           <ToasterContext />
           <Navbar />
           {children}
         </AuthContext>
+        <Footer />
       </body>
     </html>
   );
