6d0a3c6df2924fac6d8a38504eef8a9a8abf8119
lichaa824 <lisandrorubi@gmail.com>
2023-05-29 17:05:26 -0300

    Add user only badge to feature item

--- app/components/features/feature.tsx
+++ app/components/features/feature.tsx
@@ -5,11 +5,13 @@ interface FeatureProps {
     title: string
     description: string
     alt?: string
+    userOnly?: boolean
 }
 
-const Feature: React.FC<FeatureProps> = ({icon: Icon, title, description}) => {
-    return(<article className=" flex items-center flex-col w-2/3 md:w-1/3 gap-y-3 text-center">
+const Feature: React.FC<FeatureProps> = ({icon: Icon, title, description, userOnly}) => {
+    return(<article className="indicator flex items-center flex-col w-2/3 md:w-1/3 gap-y-3 text-center">
         <Icon size={32}/>
+        {userOnly && <span className="indicator-item badge badge-accent">For users only</span>  }
         <span className=" font-bold color-[#505050] text-xl">{title}</span>
         <p className="color-[#e5e7eb] text-sm sm:text-md">{description}</p>
         </article>) 
--- app/components/features/features.tsx
+++ app/components/features/features.tsx
@@ -2,13 +2,15 @@ import Feature from "./feature"
 import { MdQueryStats } from 'react-icons/md'
 import { FiLink } from 'react-icons/fi'
 import { BsHandThumbsUp } from 'react-icons/bs'
+import { BiCustomize } from 'react-icons/bi'
 
 const Features = () => {
     return (
-        <section className=" max-w-5xl flex flex-col items-center md:flex-row gap-8 mt-16">
+        <section className=" max-w-5xl flex flex-col md:flex-row gap-8 mt-16">
         <Feature icon={BsHandThumbsUp} title='Easy' description='URL Shortener is easy and fast, enter the long link to get your shortened link' />
-        <Feature icon={FiLink} title='Shortened' description='Use any link, no matter what size, ShortURL always shortens' />
-        <Feature icon={MdQueryStats} title='Statistics' description='Check the number of clicks that your shortened URL received' />
+        <Feature icon={FiLink} title='Shortened' description='Use any link, no matter what size, URL Shortener always shortens' />
+        <Feature userOnly={true} icon={MdQueryStats} title='Statistics' description='Check the number of clicks that your shortened URL received' />
+        <Feature userOnly={true} icon={BiCustomize} title="Custom Alias" description="Choose your own unique names or keywords for easy sharing and better branding." />
         </section>
     )
 }
