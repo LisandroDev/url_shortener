1e36d779137c342a70cfafec7b12bdd93ffc23a7
lichaa824 <lisandrorubi@gmail.com>
2023-05-29 17:42:14 -0300

    Add Dashboard Route

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+const Page = () => {
+  return (
+    <main className='flex flex-1 flex-col   gap-y-28 items-center py-12 sm:px-6 lg:px-8 base-100 '>
+        Test
+    </main>
+  );
+};
+
+export default Page;
