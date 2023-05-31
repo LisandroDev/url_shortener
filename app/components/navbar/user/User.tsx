d89d2b81e3722ed1abb1ce2e9c65ca8d833c0f7b
lichaa824 <lisandrorubi@gmail.com>
2023-05-31 18:27:20 -0300

    Change User to display spinner when fetching session

--- app/components/navbar/user/User.tsx
+++ app/components/navbar/user/User.tsx
@@ -1,41 +1,49 @@
 'use client';
 
-import { useState } from 'react';
 import Login from './Login';
 import { useSession } from 'next-auth/react';
-import SignIn from './Signup'
+import SignIn from './Signup';
 import { signOut } from 'next-auth/react';
+import Link from 'next/link';
+
 
 const User = () => {
-  const session = useSession();
-
-  if (session?.status === 'authenticated') {
-    return (
-      <div className='dropdown dropdown-end'>
-        <label tabIndex={0} className='btn btn-ghost btn-circle avatar'>
-          <div className='w-10 rounded-full'>
-            <img src='https://img.freepik.com/free-icon/user_318-563642.jpg?w=360' />
-          </div>
-        </label>
-        <ul
-          tabIndex={0}
-          className='mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52'
-        >
-
-          <li>
-            <a onClick={() => signOut()}>Logout</a>
-          </li>
-        </ul>
-      </div>
-    );
-  } else {
-    return (
-      <>
-        <Login />
-        <SignIn />
-      </>
-    );
+  const { status } = useSession();
+  
+  switch(status){
+    case 'loading':
+      return (<button className="btn btn-ghost loading"></button>)
+    case 'authenticated':
+      return (
+        <div className='dropdown dropdown-end'>
+          <label tabIndex={0} className='btn btn-ghost btn-circle avatar'>
+            <div className='w-10 rounded-full'>
+              <img src='https://img.freepik.com/free-icon/user_318-563642.jpg?w=360' />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className='mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52'
+          >
+            <li>
+              <Link href={'/dashboard'}>Dashboard</Link>
+            </li>
+  
+            <li>
+              <a onClick={() => signOut()}>Logout</a>
+            </li>
+          </ul>
+        </div>
+      );
+    default:
+      return (
+        <>
+          <Login />
+          <SignIn />
+        </>
+      );
   }
+
 };
 
 export default User;
