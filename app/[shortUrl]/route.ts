febc30dc72c69e181fd8fe7b33017cb066e18776
lichaa824 <lisandrorubi@gmail.com>
2023-05-31 18:25:52 -0300

    Add views counter to short url api

--- app/[shortUrl]/route.ts
+++ app/[shortUrl]/route.ts
@@ -6,16 +6,21 @@ export async function GET(request: NextRequest) {
   const origin = request.nextUrl.origin;
   const shortUrl_string = request.url.split(origin + '/')[1];
 
-  if (shortUrl_string.length !== 7) {
-    redirect('/error/404');
-  }
+  // if (shortUrl_string.length !== 7) {
+  //   redirect('/error/404');
+  // }
 
   const url_db = await prisma.shortURL.findFirst({
     where: { alias: shortUrl_string },
   });
 
-
   if (url_db) {
+
+   const updateViews = await prisma.shortURL.update({
+      where: { id: url_db.id },
+      data: { views: url_db.views + 1 },
+    });
+
     url_db.fullUrl.startsWith('https://') ||
     url_db.fullUrl.startsWith('http://')
       ? redirect(url_db.fullUrl)
