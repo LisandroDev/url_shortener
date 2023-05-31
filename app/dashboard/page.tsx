cdd7462d4f7716eeb79c3f08c02970f686ec0fc1
lichaa824 <lisandrorubi@gmail.com>
2023-05-31 18:28:00 -0300

    add getters for table info

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,33 @@
 import Table from './components/table';
 import Stats from './components/stats';
+import getStats from '../actions/getStats';
+import getUrls from '../actions/getUrls';
 
-const Page = () => {
-  return (
-    <main className='flex flex-1 flex-col   gap-y-28 items-center py-12 sm:px-6 lg:px-8 base-100 '>
-      <section>
-        <Stats />
-      </section>
-      <section>
-        <Table />
-      </section>
-    </main>
-  );
+const Page = async () => {
+  const statsInfo = await getStats();
+  const urlsArray = await getUrls();
+
+  if (statsInfo && urlsArray) {
+    return (
+      <main className='flex flex-1 flex-col   gap-y-28 items-center py-12 sm:px-6 lg:px-8 base-100 '>
+        <section>
+          <Stats
+            totalUrls={statsInfo?.totalLinks || 0}
+            views={statsInfo?.totalViews.views || 0}
+          />
+        </section>
+        <section>
+          <Table urls={urlsArray} />
+        </section>
+      </main>
+    );
+  } else {
+    return (
+      <main>
+        <p>Error</p>
+      </main>
+    );
+  }
 };
 
 export default Page;
