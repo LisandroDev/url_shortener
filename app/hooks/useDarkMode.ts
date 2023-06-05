fe13d8d9c1bbbfb16beb7bc421d9992c2aba9718
lichaa824 <lisandrorubi@gmail.com>
2023-06-05 20:46:45 -0300

    Previous selected theme now is saved to localStorage

--- app/hooks/useDarkMode.ts
+++ app/hooks/useDarkMode.ts
@@ -1,27 +1,42 @@
 import { useEffect, useState } from 'react';
 
-type themes = 'light' | 'dark';
+
+type themes = "light" | "dark";
 
 const useDarkMode = () => {
   const [theme, setTheme] = useState<themes>('light');
 
   useEffect(() => {
+    const previousSelection = localStorage.getItem('theme')
+    
     const prefersDark = window.matchMedia(
       '(prefers-color-scheme: dark)'
     ).matches;
-    if (prefersDark) {
+
+
+    if (prefersDark && !previousSelection) {
       setTheme('dark');
     }
+
+    if(previousSelection) {
+      previousSelection === "dark" ? setTheme('dark') : setTheme('light')
+    }
+
+
   }, []);
 
   useEffect(() => {
     if (document.querySelector('html')?.hasAttribute('data-theme')) {
       document.querySelector('html')?.setAttribute('data-theme', theme);
+      localStorage.setItem('theme', theme)
     }
+    
   }, [theme]);
+
 
   const toggleTheme = () => {
     theme === 'dark' ? setTheme('light') : setTheme('dark');
+  
   };
 
   return { theme, toggleTheme };
