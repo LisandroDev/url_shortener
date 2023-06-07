8c37f53d0245d82aa606a5d1fde6c8a5c5cb6710
lichaa824 <lisandrorubi@gmail.com>
2023-06-07 14:27:06 -0300

    Change default icon

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import ThemeButton from './themeButton/themeButton';
 import User from './user/User';
 import Link from 'next/link';
+import Image from 'next/image';
+import logo from './logo.svg'
 
 const Navbar = () => {
 
@@ -10,6 +12,7 @@ const Navbar = () => {
   <div className="flex-1">
   <Link href="/" className="btn btn-ghost normal-case sm:text-sm md:text-xl">
         UrlShortener
+        <Image src={logo} width={30} height={30} alt='url shortener logo' />
       </Link>
   </div>
   <div className="flex-none gap-2 mr-8">
