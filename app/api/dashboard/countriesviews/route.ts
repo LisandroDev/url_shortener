fb082f2222fabf9d48024c28e8c9d05b2fdcfbf8
lichaa824 <lisandrorubi@gmail.com>
2023-06-07 21:28:26 -0300

    Add api route to retrieve views per country

---
+++ app/api/dashboard/countriesviews/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import getCurrentUser from "@/app/api/utils/getCurrentUser";
+import { BadRequestError, UnauthorizedError } from "@/app/error/Error";
+import prisma from '@/app/libs/prismadb';
+
+export async function POST(request: Request){
+    try {
+        const body = await request.json()
+        const user = await getCurrentUser()
+
+        console.log(body)
+        const { shortUrlId } = body.data
+        
+        if(!user){
+            throw new UnauthorizedError('User is not logged in')
+        }
+
+        if(!shortUrlId){
+            throw new BadRequestError('Short URL was not provided')
+        }
+
+        const viewsPerCountry = await prisma.countryViews.findMany({where: {shortURLId: shortUrlId}})
+
+        return NextResponse.json({ viewsPerCountry: viewsPerCountry })
+
+    } catch (error) {
+        console.log('ENDPOINT: API/DASHBOARD/COUNTRIESVIEWS | METHOD:GET MESSAGE: ', error);
+        return new NextResponse('Error', { status: 500 });
+      }
+}
